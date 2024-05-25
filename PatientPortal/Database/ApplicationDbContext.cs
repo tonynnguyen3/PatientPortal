b9799c0bf23ac14fd5567cd0174590f9449ef3b3
Tony Nguyen <tonynnguyen3@gmail.com>
2024-05-25 10:55:04 -0700

    Second Commit

---
+++ PatientPortal/Database/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace PatientPortal.Database
+{
+    using Microsoft.EntityFrameworkCore;
+    public class ApplicationDbContext : DbContext
+    {
+        //Generating an DbContextOptions, by calling it super constructor method
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
---
+++ PatientPortal/Database/Patient.cs
@@ -0,0 +1,9 @@
+﻿namespace PatientPortal.Database
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
